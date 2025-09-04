9fb458941c593db984884c3366bb252e64a36866
itsuki <itsuki.enjyoy@gmail.com>
2025-09-04 17:14:07 +0900

    Initial Commit

---
+++ SpeechAnalyzerDemo/SpeechAnalyzerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechAnalyzerDemoApp.swift
+//  SpeechAnalyzerDemo
+//
+//  Created by Itsuki on 2025/09/04.
+//
+
+import SwiftUI
+
+@main
+struct SpeechAnalyzerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
